c62380bf6612623e2a9650bf2451c216115d89fb
Moritz Maerkle <mommde@protonmail.com>
2022-11-16 00:50:55 +0100

    filtered for only sshd.exe on fetching openSSH

--- src/main/java/net/kerner/entkerner/ssh/UnzipUtil.java
+++ src/main/java/net/kerner/entkerner/ssh/UnzipUtil.java
@@ -22,15 +22,18 @@ public class UnzipUtil {
             ZipEntry ze = zis.getNextEntry();
             while(ze != null){
                 String fileName = ze.getName();
+                if (!fileName.equals("sshd.exe")) continue;
+
                 File newFile = new File(destDir + File.separator + fileName);
-                System.out.println("Unzipping to "+newFile.getAbsolutePath());
-                //create directories for sub directories in zip
-                new File(newFile.getParent()).mkdirs();
+
+                System.out.println("Unzipping to " + newFile.getAbsolutePath());
+
                 FileOutputStream fos = new FileOutputStream(newFile);
                 int len;
                 while ((len = zis.read(buffer)) > 0) {
                     fos.write(buffer, 0, len);
                 }
+
                 fos.close();
                 //close this ZipEntry
                 zis.closeEntry();
